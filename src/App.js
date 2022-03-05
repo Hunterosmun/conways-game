e3f93fda1b075c1990259721f864f5f4a3e999ce
Hunter Osmun <Hunter@Osmun.net>
2022-03-05 14:14:10 -0700

    resizing works

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import styled from 'styled-components'
 
 // TO DO:
 // [x] Animation frame
-// [ ] Dragable to fill
-// [ ] Sliders for row, column, and size (also reset to default button)
-// [ ] make sure clear stops animation
+// [x] Dragable to fill
+// [x] Sliders for row, column, and size (also reset to default button)
+// [x] make sure clear stops animation
 // [ ] Option for hard edges or continuous (like dallins)
 // [ ] Maybe give options to go for so many frames (like 10 or 5 (sets of 16ms))
 
@@ -35,88 +35,106 @@ function useAnimationFrame (fn) {
   }
 }
 
-const numOfRows = 30
-const numOfCols = 20
 export default function App () {
-  const [livingThings, setLife] = React.useState([
-    ...new Array(numOfRows * numOfCols).fill(false)
-  ])
+  const [numOfRows, setRows] = React.useState(30)
+  const [numOfCols, setCols] = React.useState(20)
+  const [size, setSize] = React.useState(24)
+  const [livingThings, setLife] = React.useState([])
   const animation = useAnimationFrame(step)
+
+  React.useEffect(() => {
+    setLife([...new Array(numOfRows * numOfCols).fill(false)])
+  }, [numOfCols, numOfRows])
+
+  const refRows = React.useRef()
+  refRows.current = numOfRows
+  const refLiv = React.useRef()
+  refLiv.current = livingThings
 
   // here we could have used React.useRef and set the ref.current to be living things
   function step () {
-    setLife(actualLivingThings => {
-      // y = position / numOfRows
-      // x = position % numOfRows
-      let newLife = [...actualLivingThings]
-      newLife = newLife.map((el, i) => {
-        let livingTouch = 0
+    console.log({ 1: livingThings.length })
+    // y = position / numOfRows
+    // x = position % numOfRows
+    const numOfRows = refRows.current
+    console.log({ 2: numOfRows })
+    let oldLife = refLiv.current
+    console.log({ 3: oldLife.length })
+    let newLife = oldLife.map((el, i) => {
+      let livingTouch = 0
 
-        // in this section the top is known
-        if (i > numOfRows) {
-          // top
-          if (actualLivingThings[i - numOfRows] === true) livingTouch += 1
-          // top-right
-          if (i % numOfRows !== 0) {
-            if (actualLivingThings[i - numOfRows - 1] === true) livingTouch += 1
-          }
-          // top-left
-          if (i % numOfRows !== 1) {
-            if (actualLivingThings[i - numOfRows + 1] === true) livingTouch += 1
-          }
-        }
-
-        // right
+      // in this section the top is known
+      if (i > numOfRows) {
+        // top
+        if (oldLife[i - numOfRows] === true) livingTouch += 1
+        // top-right
         if (i % numOfRows !== 0) {
-          if (actualLivingThings[i + 1] === true) livingTouch += 1
+          if (oldLife[i - numOfRows - 1] === true) livingTouch += 1
         }
-
-        // left
+        // top-left
         if (i % numOfRows !== 1) {
-          if (actualLivingThings[i - 1] === true) livingTouch += 1
+          if (oldLife[i - numOfRows + 1] === true) livingTouch += 1
         }
+      }
 
+      // right
+      if (i % numOfRows !== 0) {
+        if (oldLife[i + 1] === true) livingTouch += 1
+      }
+
+      // left
+      if (i % numOfRows !== 1) {
+        if (oldLife[i - 1] === true) livingTouch += 1
+      }
+
+      // bottom
+      if (i <= oldLife.length - numOfRows) {
         // bottom
-        if (i <= actualLivingThings.length - numOfRows) {
-          // bottom
-          if (actualLivingThings[i + numOfRows] === true) livingTouch += 1
-          // bottom-right
-          if (i % numOfRows !== 0) {
-            if (actualLivingThings[i + numOfRows + 1] === true) livingTouch += 1
-          }
-          // bottom-left
-          if (i % numOfRows !== 1) {
-            if (actualLivingThings[i + numOfRows - 1] === true) livingTouch += 1
-          }
+        if (oldLife[i + numOfRows] === true) livingTouch += 1
+        // bottom-right
+        if (i % numOfRows !== 0) {
+          if (oldLife[i + numOfRows + 1] === true) livingTouch += 1
         }
+        // bottom-left
+        if (i % numOfRows !== 1) {
+          if (oldLife[i + numOfRows - 1] === true) livingTouch += 1
+        }
+      }
 
-        // 1) Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-        if (el === true && livingTouch < 2) return false
+      // 1) Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+      if (el === true && livingTouch < 2) return false
 
-        // 2) Any live cell with two or three live neighbours lives on to the next generation.
-        if (el === true && (livingTouch === 3 || livingTouch === 2)) return true
+      // 2) Any live cell with two or three live neighbours lives on to the next generation.
+      if (el === true && (livingTouch === 3 || livingTouch === 2)) return true
 
-        // 3) Any live cell with more than three live neighbours dies, as if by overpopulation.
-        if (el === true && livingTouch > 3) return false
+      // 3) Any live cell with more than three live neighbours dies, as if by overpopulation.
+      if (el === true && livingTouch > 3) return false
 
-        // 4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-        if (el === false && livingTouch === 3) return true
+      // 4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+      if (el === false && livingTouch === 3) return true
 
-        return false
-      })
-
-      return newLife
+      return false
     })
+
+    refLiv.current = newLife
+    setLife(newLife)
   }
 
   return (
     <Background>
       <TopBox>Conways Game of Life </TopBox>
-      <GridBlock>
+      <GridBlock numOfRows={numOfRows} size={size}>
         {livingThings.map((el, i) => (
           <Block
             key={i}
             active={el}
+            size={size}
+            onMouseOver={e => {
+              if (!e.buttons) return
+              let newLiving = [...livingThings]
+              newLiving[i] = !el
+              setLife(newLiving)
+            }}
             onClick={() => {
               let newLiving = [...livingThings]
               newLiving[i] = !el
@@ -167,6 +185,44 @@ export default function App () {
         >
           Clear
         </button>
+        <label>Rows</label>
+        <input
+          style={{ width: '90px' }}
+          type='range'
+          value={numOfRows}
+          onChange={e => {
+            setRows(e.target.valueAsNumber)
+          }}
+        />
+        <label>Columns</label>
+        <input
+          style={{ width: '90px' }}
+          type='range'
+          value={numOfCols}
+          onChange={e => {
+            setCols(e.target.valueAsNumber)
+          }}
+          name={numOfCols}
+        />
+        <label>Box Size</label>
+        <input
+          style={{ width: '90px' }}
+          type='range'
+          value={size}
+          onChange={e => {
+            setSize(e.target.value)
+          }}
+          name={size}
+        />
+        <button
+          onClick={() => {
+            setSize(24)
+            setCols(20)
+            setRows(30)
+          }}
+        >
+          Default
+        </button>
       </BtnBox>
     </Background>
   )
@@ -175,13 +231,13 @@ export default function App () {
 const Block = styled.div`
   background-color: ${p => (p.active ? 'black' : 'white')};
   box-sizing: border-box;
-  width: 24px;
-  height: 24px;
+  width: ${p => p.size}px;
+  height: ${p => p.size}px;
   border: 1px solid black;
 `
 
 const GridBlock = styled.div`
-  width: ${24 * numOfRows}px;
+  width: ${p => p.size * p.numOfRows}px;
   padding-left: 130px;
   display: flex;
   flex-wrap: wrap;
@@ -194,7 +250,7 @@ const BtnBox = styled.div`
   justify-content: space-between;
   display: flex;
   flex-direction: column;
-  height: 180px;
+  height: 380px;
   button {
     padding: 8px 12px;
     margin-right: 8px;
